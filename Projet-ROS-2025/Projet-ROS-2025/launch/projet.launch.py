1cefb2811babda02c4b852bd2037712d39863082
Kevin Asmar <kevenasmar@gmail.com>
2025-04-07 14:32:37 +0200

    bon launch file

--- Projet-ROS-2025/Projet-ROS-2025/launch/projet.launch.py
+++ Projet-ROS-2025/Projet-ROS-2025/launch/projet.launch.py
@@ -1,48 +1,40 @@
-import os
-
 from launch import LaunchDescription
+from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
+from launch.substitutions import ThisLaunchFileDir
+import os
+
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # Paths to packages
-    tb3_gazebo_dir = get_package_share_directory('turtlebot3_gazebo')
-
-    # Launch Gazebo simulation with the robot
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(tb3_gazebo_dir, 'launch', 'empty_world.launch.py')
-        )
-    )
-
-    # Custom teleop node
-    teleop_node = Node(
-        package='bachir_gemayel',
-        executable='bachir_gemayel_node',
-        name='bachir_gemayel',
-        output='screen',
-        emulate_tty=True,
-        parameters=[
-            {'linear_scale': 1.0},
-            {'angular_scale': 1.0},
-            {'use_sim_time': True}
-        ]
-    )
-
-    auto_stop = Node(
-        package='mybot_control',
-        executable='lds_distance',  # or 'auto_stop' if you named it differently
-        name='lds_distance',
-        output='screen',
-        parameters=[
-            {'use_sim_time': True}
-        ]
+    # Path to the projet2025 world launch file
+    projet2025_launch_path = os.path.join(
+        get_package_share_directory('projet2025'),
+        'launch',
+        'projet.launch.py'
     )
 
     return LaunchDescription([
-        gazebo,
-        teleop_node
-        auto_stop
+        # Include the projet2025 simulation world
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(projet2025_launch_path)
+        ),
+
+        # Node for teleoperation
+        Node(
+            package='mybot_teleop',
+            executable='turtlebot_teleop_node',
+            name='turtlebot_teleop_node',
+            output='screen'
+        ),
+
+        # Node for line following
+        Node(
+            package='Projety-ROS-2025',
+            executable='line_following',
+            name='line_following_node',
+            output='screen',
+            parameters=[{'roundabout_direction': 'left'}]  # tu peux changer par 'right'
+        ),
     ])
